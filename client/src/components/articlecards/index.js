09690c1398c2c7df55b805dd4a85c280e2e2a387
Jacob Moss <jacobmoss09@gmail.com>
2019-10-06 17:20:39 -0500

    hrefs for articles working as expected

--- client/src/components/articlecards/index.js
+++ client/src/components/articlecards/index.js
@@ -4,10 +4,10 @@ import { Card, Button, CardTitle, CardText } from 'reactstrap';
 const ArticleCard = (props) => {
   return (
     <div>
-      <Card body className="text-center">
+      <Card body className="text-center col-sm-10 offset-sm-1">
         <CardTitle>{props.title}</CardTitle>
         <CardText>{props.summary}</CardText>
-        <Button href={props.href}>Read More</Button>
+        <Button href={'https://na.leagueoflegends.com' + props.href} target='_blank'>Read More</Button>
       </Card>
     </div>
   );
